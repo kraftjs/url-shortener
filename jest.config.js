a281a2bb56a12ecbcc8b7af3a3261fb2648ff075
Jason Kraft <developer.kraft@gmail.com>
2021-08-03 23:07:33 -0700

    Change website name to heroku dyno and update CSS; remove unnecessary jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const config = {
     preset: 'ts-jest',
     clearMocks: true,
     testEnvironment: 'node',
-    globalSetup: '<rootDir>/test/migrateDb.ts',
     setupFilesAfterEnv: ['<rootDir>/test/truncateDb.ts', '<rootDir>/test/seedDb.ts', '<rootDir>/test/disconnectDb.ts'],
 };
 
