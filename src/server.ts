1802fa3469145422a04be82b4f4bec491e0963ee
Jason Kraft <developer.kraft@gmail.com>
2021-08-03 21:57:42 -0700

    Fix port for Heroku

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import app from './app';
 
 const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
 const server = process.env.NODE_ENV === 'production' ? https.createServer(app) : http.createServer(app);
-const port = process.env.NODE_ENV === 'production' ? 8443 : 8080;
+const port: number = process.env.PORT as unknown as number || 8080;
 
 server.listen(port, hostname(), () => {
     console.log(`Server running at ${protocol}://${hostname()}:${port}/`);
