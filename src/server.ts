3aa38ca7465a17a70e64861e084323da07116f90
Jason Kraft <developer.kraft@gmail.com>
2021-08-03 22:03:51 -0700

    Change server to http

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { hostname } from 'os';
-import https from 'https';
 import http from 'http';
 
 import app from './app';
 
-const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-const server = process.env.NODE_ENV === 'production' ? https.createServer(app) : http.createServer(app);
-const port: number = process.env.PORT as unknown as number || 8080;
+const protocol = 'http';
+const server = http.createServer(app);
+const port: number = (process.env.PORT as unknown as number) || 8080;
 
 server.listen(port, hostname(), () => {
     console.log(`Server running at ${protocol}://${hostname()}:${port}/`);
